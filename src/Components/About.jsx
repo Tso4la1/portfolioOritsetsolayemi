93feb477c8590e8793255adc169ad02d96482f90
Tso4la1 <tsolajohn@gmail.com>
2024-01-01 02:02:12 +0100

    Header, navbar, about and footer corrections

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -19,8 +19,11 @@ export const About = () => {
                         <div className="about_info">
                             <h1>Creative Ui & Ux Design <br /> based in Delta State, Nigeria</h1>
                             <div className="about_info">
-                                <p>One of my key strengths lies in my proficiency with modern CSS frameworks, particularly Tailwind CSS and Styled-Components.  Leveraging these tools, I've streamlined the development process,  resulting in more efficient code and accelerated project timelines. Additionally, my experience with React has allowed me to build scalable and modular components,contributing to the development of  robust and maintainable codebases.
-                                </p>
+                                <div className="about-info-p">
+                                    <p>One of my key strengths lies in my proficienvy with modern CSS frameworks,
+                                        particularly Tailwind CSS and Styled-Components. Leveraging these tools, I've streamlined the development process, resuilting in more efficient code and accelerated project timelines. Additionally, my experience with React has allowed me to build scalable and modular components, contributinf to the development of robust and maintainable codebases.
+                                    </p>
+                                </div>
                                 <div className="about_buttons">
                                     <a href="#about" className="btn btn-outline">
                                         Download CV
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
                     <div className="col-6">
                         <div className="header_content">
                             <div className="header-section">
-                                <h2>Hello, I Am</h2>
+                                <h2>Hello, I am</h2>
                                 <h1>Omayuku Oritsetsolayemi</h1>
                                 <p> As a seasoned developer, my expertise lies in crafting seamless and visually appealing user interfaces using a stack of technologies that includes JavaScript, HTML, CSS, Tailwind CSS, Styled-Components, and React.</p>
                                 <div className="header_buttons">
